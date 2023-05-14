b395cc2b6ab532f10d75c58057a59363aeea18e0
DaRadl <daniel.radl@ebcont.com>
2023-05-14 23:34:36 +0200

    adds second api test validating status und body, adds schema for schema validation

--- test/index.test.js
+++ test/index.test.js
@@ -1,15 +1,47 @@
 const assert = require('assert');
+
+// Unit testing module constants by rewire dependency
 const rewire = require("rewire");
 const app = rewire('../src/index.js');
 
+// API testing dependencies using chai
+const request = require("request");
+const chai = require('chai');
+const expect = require("chai").expect;
+chai.use(require('chai-json-schema'));
 
-describe('App', () => {
+describe('App Unit Test', () => {
     describe('#generateId()', () => {
-        it('Should return Ids', () => {
+        it('Should return generated max Id', () => {
             const notes = app.__get__("notes")
             assert.equal(app.__get__("generateId()"), notes.length + 1)
         });
 
     });
+});
 
+describe('App Api Tests', () => {
+    let url = "http://localhost:3001/"
+
+    describe('GET /', () => {
+        it('Should return a successful Hello World! HTTP GET', () => {
+            request(url, function (err, response){
+                expect(response.statusCode).to.equal(200);
+                expect(response.body).to.contain("Hello World!");
+            });
+        });
+
+    });
+
+    describe('Schema Validation /api/notes', () => {
+        const notesSchemaV1 = require('./resources/notes_schema_v1.json')
+
+
+        it('Should validate the received json against the provided schema', () => {
+            request(url + "api/notes", function (err, response){
+                expect(response.body).to.be.jsonSchema(notesSchemaV1);
+            });
+        });
+
+    });
 });
